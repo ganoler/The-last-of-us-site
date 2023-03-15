ab63282056b4ec3549254da57c77790bc3154164
Mortcord <ratynsky98@gmail.com>
2023-03-15 21:35:49 +0200

    V2.1

---
+++ theLastOfUs/js/index.js
@@ -0,0 +1,6 @@
+function changeLanguageUA(){
+    document.getElementById('gameplay-header').innerText="Геймплей";
+    document.getElementById('plot-header').innerText="Сюжет";
+    document.getElementById('history-header').innerText="Історія створення";
+    document.getElementById('music-header').innerText="Музика";
+}
